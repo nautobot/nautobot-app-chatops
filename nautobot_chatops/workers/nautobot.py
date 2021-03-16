a4a6b41f7f0d99f1523a1e077dc0b87ffdf7ac46
Matt Vitale <matt.vitale@networktocode.com>
2021-03-16 10:08:03 -0400

    Blacken file

--- nautobot_chatops/workers/nautobot.py
+++ nautobot_chatops/workers/nautobot.py
@@ -380,20 +380,30 @@ def get_interface_connections(dispatcher, filter_type, filter_value_1, filter_va
         if ("device" or "site") in filter_type:
             # Since the device filter prompts the user to pick a site first in order to further query devices located in the chosen site, the device filter will start off with choices of all the sites with one or more devices.
             choices = [
-                (site.name, site.slug) for site in Site.objects.annotate(Count("devices")).filter(devices__count__gt=0).order_by("name", "name")
+                (site.name, site.slug)
+                for site in Site.objects.annotate(Count("devices"))
+                .filter(devices__count__gt=0)
+                .order_by("name", "name")
             ]
         elif filter_type == "role":
             choices = [
                 (role.name, role.slug)
-                for role in DeviceRole.objects.annotate(Count("devices")).filter(devices__count__gt=0).order_by("name", "name")
+                for role in DeviceRole.objects.annotate(Count("devices"))
+                .filter(devices__count__gt=0)
+                .order_by("name", "name")
             ]
         elif filter_type == "region":
             choices = [
                 (region.name, region.slug)
-                for region in Region.objects.annotate(Count("sites")).filter(sites__count__gt=0).order_by("name", "name")
+                for region in Region.objects.annotate(Count("sites"))
+                .filter(sites__count__gt=0)
+                .order_by("name", "name")
             ]
         elif filter_type == "model":
-            choices = [(device_type.display_name, device_type.slug) for device_type in DeviceType.objects.all().order_by("model", "model")]
+            choices = [
+                (device_type.display_name, device_type.slug)
+                for device_type in DeviceType.objects.all().order_by("model", "model")
+            ]
         elif filter_type == "all":
             # 1 param slash command
             connections = (
