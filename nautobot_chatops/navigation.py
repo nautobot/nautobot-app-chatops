76ce10832c7b22d27ab7500781c3520a8210c4e5
carbonarok <carbonarok@gmail.com>
2021-07-08 15:33:53 +0100

    Top level navigation added.

--- nautobot_chatops/navigation.py
+++ nautobot_chatops/navigation.py
@@ -1,5 +1,5 @@
 """Plugin additions to the Nautobot navigation menu."""
-
+from nautobot.core.apps import NavMenuAddButton, NavMenuGroup, NavMenuItem, NavMenuImportButton, NavMenuTab
 from nautobot.extras.plugins import PluginMenuItem, PluginMenuButton
 from nautobot.utilities.choices import ButtonColorChoices
 
@@ -36,5 +36,44 @@ menu_items = (
         link="plugins:nautobot_chatops:home",
         link_text="Command Usage Records",
         permissions=["nautobot_chatops.view_commandlog"],
+    ),
+    NavMenuTab(
+        name="ChatOps",
+        weight=1200,
+        groups=(
+            NavMenuGroup(
+                name="ChatOps",
+                weight=100,
+                items=(
+                    NavMenuItem(
+                        link="plugins:nautobot_chatops:accessgrant_list",
+                        name="Access Grants",
+                        permissions=["nautobot_chatops.view_commandtoken"],
+                        buttons=(
+                            NavMenuAddButton(
+                                link="plugins:nautobot_chatops:accessgrant_add",
+                                permissions=["nautobot_chatops.add_accessgrant"],
+                            ),
+                        ),
+                    ),
+                    NavMenuItem(
+                        link="plugins:nautobot_chatops:commandtoken_list",
+                        name="Command Tokens",
+                        permissions=["nautobot_chatops.view_commandtoken"],
+                        buttons=(
+                            NavMenuAddButton(
+                                link="plugins:nautobot_chatops:commandtoken_add",
+                                permissions=["nautobot_chatops.add_commandtoken"],
+                            ),
+                        ),
+                    ),
+                    NavMenuItem(
+                        link="plugins:nautobot_chatops:home",
+                        name="Command Usage Records",
+                        permissions=["nautobot_chatops.view_commandlog"],
+                    ),
+                ),
+            ),
+        ),
     ),
 )
