02d44b1d8ee77272ede74f6de01bdf46c346d55c
Stephen Kiely <stephenkiely87@gmail.com>
2021-08-14 14:22:56 -0500

    Use deepcopy for data migration.

--- nautobot_chatops/migrations/0003_params_to_params1.py
+++ nautobot_chatops/migrations/0003_params_to_params1.py
@@ -1,23 +1,20 @@
+import copy
+import json
+
 from django.db import migrations, connection
 
 def migrate_params(apps, schema_editor):
     """Migrate params field to params1"""
     CommandLog = apps.get_model('nautobot_chatops', 'CommandLog')
     for log in CommandLog.objects.all():
-        params_list = list
-        for key, value in log.params:
-            params_list.append([key, value])
-        log.params1 = params_list
+        log.params1 = copy.deepcopy(log.params)
         log.save()
 
 def reverse_migrate_params(apps, schema_editor):
     """Reverse migration of params to params1."""
     CommandLog = apps.get_model('nautobot_chatops', 'CommandLog')
     for log in CommandLog.objects.all():
-        params_list = list
-        for key, value in log.params1:
-            params_list.append([key, value])
-        log.params = params_list
+        log.params = copy.deepcopy(log.params1)
         log.save()
 
 class Migration(migrations.Migration):
