acf91185499aa7588a3cd1550207ff07b305ddaa
Josh VanDeraa <jv@networktocode.com>
2022-02-02 21:02:38 -0600

    Adds comment out for celery inclusion.

--- tasks.py
+++ tasks.py
@@ -46,7 +46,12 @@ namespace.configure(
             "python_ver": "3.6",
             "local": False,
             "compose_dir": os.path.join(os.path.dirname(__file__), "development"),
-            "compose_files": ["docker-compose.requirements.yml", "docker-compose.celery.yml", "docker-compose.base.yml", "docker-compose.dev.yml"],
+            "compose_files": [
+                "docker-compose.requirements.yml",
+                # "docker-compose.celery.yml",
+                "docker-compose.base.yml",
+                "docker-compose.dev.yml",
+            ],
         }
     }
 )
