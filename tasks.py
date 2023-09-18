7a655bd36871cbc7ddf2d6a74c5d31b96f6180d3
Stephen Kiely <stephenkiely87@gmail.com>
2023-09-18 15:58:19 -0500

    Prepping for RC

--- tasks.py
+++ tasks.py
@@ -45,7 +45,7 @@ namespace = Collection("nautobot_chatops")
 namespace.configure(
     {
         "nautobot_chatops": {
-            "nautobot_ver": "2.0.0-rc.2",
+            "nautobot_ver": "2.0.0-rc.3",
             "project_name": "nautobot-chatops",
             "python_ver": "3.10",
             "local": False,
