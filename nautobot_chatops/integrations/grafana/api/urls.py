0d7060a6971f44dc8f6ac513cd5c87e88f9b37dd
Stephen Kiely <stephenkiely87@gmail.com>
2023-09-22 15:25:10 -0500

    Remove Grafana urls and navigation if disabled.

--- nautobot_chatops/integrations/grafana/api/urls.py
+++ nautobot_chatops/integrations/grafana/api/urls.py
@@ -1,16 +1,16 @@
 """Django urlpatterns declaration for nautobot_chatops.integrations.grafana plugin."""
 
-# from django.urls import path
+from django.conf import settings
 
 from nautobot.core.api import OrderedDefaultRouter
 from nautobot_chatops.integrations.grafana.api.views.generic import NautobotPluginChatopsGrafanaRootView
 
 
 urlpatterns = []
+if settings.PLUGINS_CONFIG["nautobot_chatops"]["enable_grafana"]:
+    router = OrderedDefaultRouter()
+    router.APIRootView = NautobotPluginChatopsGrafanaRootView
 
-router = OrderedDefaultRouter()
-router.APIRootView = NautobotPluginChatopsGrafanaRootView
+    app_name = "nautobot_chatops.grafana-api"
 
-app_name = "nautobot_chatops.grafana-api"
-
-urlpatterns += router.urls
+    urlpatterns += router.urls
