6c0eece405a05ae8c502c9fe757ef2a58a66ceb1
Stephen Kiely <stephenkiely87@gmail.com>
2023-09-29 15:03:14 -0500

    Black

--- nautobot_chatops/__init__.py
+++ nautobot_chatops/__init__.py
@@ -121,33 +121,21 @@ class NautobotChatOpsConfig(NautobotAppConfig):
         "panorama_user": "",
     }
     constance_config = {
-        "fallback_chatops_user": ConstanceConfigItem(
-            default="chatbot", help_text="Enable Mattermost Chat Platform."
-        ),
+        "fallback_chatops_user": ConstanceConfigItem(default="chatbot", help_text="Enable Mattermost Chat Platform."),
         "enable_mattermost": ConstanceConfigItem(
             default=False, help_text="Enable Mattermost Chat Platform.", field_type=bool
         ),
         "enable_ms_teams": ConstanceConfigItem(
             default=False, help_text="Enable Microsoft Teams Chat Platform.", field_type=bool
         ),
-        "enable_slack": ConstanceConfigItem(
-            default=False, help_text="Enable Slack Chat Platform.", field_type=bool
-        ),
-        "enable_webex": ConstanceConfigItem(
-            default=False, help_text="Enable Webex Chat Platform.", field_type=bool
-        ),
-        "enable_aci": ConstanceConfigItem(
-            default=False, help_text="Enable Cisco ACI Integration.", field_type=bool
-        ),
-        "enable_ansible": ConstanceConfigItem(
-            default=False, help_text="Enable Ansible Integration.", field_type=bool
-        ),
+        "enable_slack": ConstanceConfigItem(default=False, help_text="Enable Slack Chat Platform.", field_type=bool),
+        "enable_webex": ConstanceConfigItem(default=False, help_text="Enable Webex Chat Platform.", field_type=bool),
+        "enable_aci": ConstanceConfigItem(default=False, help_text="Enable Cisco ACI Integration.", field_type=bool),
+        "enable_ansible": ConstanceConfigItem(default=False, help_text="Enable Ansible Integration.", field_type=bool),
         "enable_aristacv": ConstanceConfigItem(
             default=False, help_text="Enable Arista CloudVision Integration.", field_type=bool
         ),
-        "enable_grafana": ConstanceConfigItem(
-            default=False, help_text="Enable Grafana Integration.", field_type=bool
-        ),
+        "enable_grafana": ConstanceConfigItem(default=False, help_text="Enable Grafana Integration.", field_type=bool),
         "enable_ipfabric": ConstanceConfigItem(
             default=False, help_text="Enable IP Fabric Integration.", field_type=bool
         ),
