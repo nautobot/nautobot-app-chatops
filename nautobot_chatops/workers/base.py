74a6071d3627f7e5e6a7542925e5a122f3b8f7dc
Stephen Kiely <stephenkiely87@gmail.com>
2024-08-23 22:56:36 -0500

    Update base.py
    Add back in he pylint disable

--- nautobot_chatops/workers/base.py
+++ nautobot_chatops/workers/base.py
@@ -6,6 +6,7 @@ Obsolete - contents of this file have been migrated to other modules.
 import warnings
 
 # For backwards compatibility -- these used to be defined in this file
+# pylint: disable=unused-import
 from nautobot_chatops.utils import create_command_log  # noqa: F401
 from nautobot_chatops.workers import subcommand_of, handle_subcommands  # noqa: F401
 
