3a83a6ae69ac0f6017b1cc3c71ac569d88642147
Stephen Kiely <stephenkiely87@gmail.com>
2024-12-18 16:12:51 -0600

    Resolve synchronous function running in the Async Slack Socket Mode (#350)
    * Wrap get_commands_registry with sync_to_async
    
    * Move Pull Request Template so that Github picks this up.
    
    * Update changes/348.fixed
    
    Co-authored-by: Gary Snider <75227981+gsnider2195@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Gary Snider <75227981+gsnider2195@users.noreply.github.com>

--- nautobot_chatops/sockets/slack.py
+++ nautobot_chatops/sockets/slack.py
@@ -4,6 +4,7 @@ import asyncio
 import json
 import shlex
 
+from asgiref.sync import sync_to_async
 from django.conf import settings
 from slack_sdk.socket_mode.aiohttp import SocketModeClient
 from slack_sdk.socket_mode.request import SocketModeRequest
@@ -71,7 +72,7 @@ async def main():
             client.logger.error("%s", err)
             return
 
-        registry = get_commands_registry()
+        registry = await sync_to_async(get_commands_registry)()
 
         if command not in registry:
             SlackDispatcher(context).send_markdown(commands_help(prefix=SLASH_PREFIX))
@@ -210,7 +211,7 @@ async def main():
 
         client.logger.info(f"command: {command}, subcommand: {subcommand}, params: {params}")
 
-        registry = get_commands_registry()
+        registry = await sync_to_async(get_commands_registry)()
 
         if command not in registry:
             SlackDispatcher(context).send_markdown(commands_help(prefix=SLASH_PREFIX))
@@ -241,7 +242,7 @@ async def main():
             client.logger.error("%s", err)
             return
 
-        registry = get_commands_registry()
+        registry = await sync_to_async(get_commands_registry)()
 
         if command not in registry:
             SlackDispatcher(context).send_markdown(commands_help(prefix=SLASH_PREFIX))
