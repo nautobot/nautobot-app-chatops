9726c9634e9422dd9eef4220f41c02ed375f9d2b
Stephen Kiely <stephenkiely87@gmail.com>
2025-10-07 15:45:17 -0500

    Simplify tables and cleanup access grant create.

--- nautobot_chatops/models.py
+++ nautobot_chatops/models.py
@@ -58,6 +58,8 @@ class CommandLog(PrimaryModel):  # pylint: disable=nb-string-field-blank-null
         null=True,
         related_name="command_log",
     )
+    is_dynamic_group_associable_model = False  # Disable dynamic group association for this model
+    is_contact_associable_model = False  # Disable contact association for this model
 
     @property
     def status_label_class(self):
@@ -102,6 +104,8 @@ class AccessGrant(PrimaryModel):
         max_length=255,
         help_text=ACCESS_GRANT_VALUE_HELP_TEXT,
     )
+    is_dynamic_group_associable_model = False  # Disable dynamic group association for this model
+    is_contact_associable_model = False  # Disable contact association for this model
 
     def clean(self):
         """Model validation logic."""
@@ -126,6 +130,8 @@ class CommandToken(PrimaryModel):
     comment = models.CharField(max_length=255, help_text=COMMAND_TOKEN_COMMENT_HELP_TEXT, blank=True, default="")
     platform = models.CharField(max_length=32, choices=PlatformChoices)
     token = models.CharField(max_length=255, help_text=COMMAND_TOKEN_TOKEN_HELP_TEXT)
+    is_dynamic_group_associable_model = False  # Disable dynamic group association for this model
+    is_contact_associable_model = False  # Disable contact association for this model
 
     def __str__(self):
         """String representation of a CommandToken."""
@@ -150,6 +156,8 @@ class ChatOpsAccountLink(PrimaryModel):
     platform = models.CharField(max_length=32, choices=PlatformChoices)
     user_id = models.CharField(max_length=255, help_text=CHATOPS_USER_ID_HELP_TEXT, verbose_name="Chat User ID")
     email = models.EmailField(blank=True)
+    is_dynamic_group_associable_model = False  # Disable dynamic group association for this model
+    is_contact_associable_model = False  # Disable contact association for this model
 
     def __str__(self):
         """String representation of a ChatOps Account Link."""
--- nautobot_chatops/tables.py
+++ nautobot_chatops/tables.py
@@ -1,5 +1,6 @@
 """Django table classes for Nautobot."""
 
+from django.utils.html import format_html, format_html_join
 from django_tables2 import Column, LinkColumn, TemplateColumn
 from nautobot.core.tables import BaseTable, ButtonsColumn, ToggleColumn
 
@@ -17,29 +18,10 @@ class CommandLogTable(BaseTable):
     """Table for rendering a listing of CommandLog entries."""
 
     pk = ToggleColumn()
-    runtime = TemplateColumn(template_code="{{ record.runtime | shorter_timedelta }}")
 
     # pylint: disable=line-too-long
     platform = TemplateColumn(
         template_code='<span class="label" style="background-color: #{{ record.platform_color }}">{{ record.platform }}</span>'
-    )
-
-    command = TemplateColumn(template_code='<span style="font-family: monospace">{{ record.command }}</span>')
-    subcommand = TemplateColumn(
-        template_code='<span style="font-family: monospace">'
-        "{% if record.subcommand %}{{ record.subcommand }}{% else %}&mdash;{% endif %}</span>"
-    )
-    params = TemplateColumn(
-        template_code="""
-{% for key, value in record.params %}
-<div><strong>{{ key }}</strong>:&nbsp;{{ value }}</div>
-{% empty %}
-&mdash;
-{% endfor %}""",
-    )
-
-    status = TemplateColumn(
-        template_code='<span class="label label-{{record.status_label_class}}">{{ record.status }}</span>'
     )
 
     class Meta(BaseTable.Meta):
@@ -60,19 +42,23 @@ class CommandLogTable(BaseTable):
         )
         default_columns = ("start_time", "user_name", "nautobot_user", "command", "subcommand", "params", "status")
 
+    def render_params(self, record):
+        """Render the params column."""
+        if record.params:
+            return format_html_join("", "<strong>{}</strong>:&nbsp;{}<br>", ((key, value) for key, value in record.params))
+        return self.default
+
+    def render_status(self, record):
+        """Render the status column."""
+        if record.status:
+            return format_html("<span class='label label-{}'>{}</span>", record.status_label_class, record.status)
+        return self.default
+
 
 class AccessGrantTable(BaseTable):
     """Table for rendering a listing of AccessGrant entries."""
 
     pk = ToggleColumn()
-
-    command = TemplateColumn(template_code='<span style="font-family: monospace">{{ record.command }}</span>')
-
-    subcommand = TemplateColumn(template_code='<span style="font-family: monospace">{{ record.subcommand }}</span>')
-
-    grant_type = TemplateColumn(template_code='<span class="label label-success">{{ record.grant_type }}</span>')
-
-    value = TemplateColumn(template_code='<span style="font-family: monospace">{{ record.value }}</span>')
     name = Column(linkify=True)
 
     actions = ButtonsColumn(AccessGrant)
@@ -81,29 +67,23 @@ class AccessGrantTable(BaseTable):
         """Metaclass attributes of AccessGrantTable."""
 
         model = AccessGrant
-        fields = ("pk", "command", "subcommand", "grant_type", "name", "value", "actions")
-        default_columns = ("pk", "command", "subcommand", "grant_type", "name", "actions")
+        fields = ("pk", "name", "value", "command", "subcommand", "grant_type", "actions")
+        default_columns = ("pk", "name", "command", "subcommand", "grant_type", "actions")
 
 
 class CommandTokenTable(BaseTable):
     """Table for rendering a listing of CommandToken entries."""
 
     pk = ToggleColumn()
-
-    platform = TemplateColumn(template_code='<span style="font-family: monospace">{{ record.platform }}</span>')
-
     token = Column(linkify=True)
-
-    comment = TemplateColumn(template_code='<span style="font-family: monospace">{{ record.comment }}</span>')
-
     actions = ButtonsColumn(CommandToken)
 
     class Meta(BaseTable.Meta):
         """Metaclass attributes of CommandTokenTable."""
 
         model = CommandToken
-        fields = ("pk", "platform", "token", "comment", "actions")
-        default_columns = ("pk", "platform", "token", "comment", "actions")
+        fields = ("pk", "token", "platform", "comment", "actions")
+        default_columns = ("pk", "token", "platform", "comment", "actions")
 
 
 class ChatOpsAccountLinkTable(BaseTable):
