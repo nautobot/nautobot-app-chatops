f804e7a0bf764e9a9e63ced1eb632887e19786a2
Stephen Kiely <stephenkiely87@gmail.com>
2025-11-06 10:22:58 -0600

    Nav changes

--- nautobot_chatops/navigation.py
+++ nautobot_chatops/navigation.py
@@ -1,6 +1,6 @@
 """App additions to the Nautobot navigation menu."""
 
-from nautobot.apps.ui import NavMenuAddButton, NavMenuGroup, NavMenuItem, NavMenuTab
+from nautobot.apps.ui import NavigationIconChoices, NavigationWeightChoices, NavMenuGroup, NavMenuItem, NavMenuTab
 
 from nautobot_chatops.integrations.grafana.navigation import items as grafana_items
 
@@ -8,39 +8,25 @@ items = [
     NavMenuItem(
         link="plugins:nautobot_chatops:accessgrant_list",
         name="Access Grants",
+        weight=100,
         permissions=["nautobot_chatops.view_accessgrant"],
-        buttons=(
-            NavMenuAddButton(
-                link="plugins:nautobot_chatops:accessgrant_add",
-                permissions=["nautobot_chatops.add_accessgrant"],
-            ),
-        ),
     ),
     NavMenuItem(
         link="plugins:nautobot_chatops:chatopsaccountlink_list",
         name="Link ChatOps Account",
+        weight=200,
         permissions=["nautobot_chatops.view_chatopsaccountlink"],
-        buttons=(
-            NavMenuAddButton(
-                link="plugins:nautobot_chatops:chatopsaccountlink_add",
-                permissions=["nautobot_chatops.add_chatopsaccountlink"],
-            ),
-        ),
     ),
     NavMenuItem(
         link="plugins:nautobot_chatops:commandtoken_list",
         name="Command Tokens",
+        weight=300,
         permissions=["nautobot_chatops.view_commandtoken"],
-        buttons=(
-            NavMenuAddButton(
-                link="plugins:nautobot_chatops:commandtoken_add",
-                permissions=["nautobot_chatops.add_commandtoken"],
-            ),
-        ),
     ),
     NavMenuItem(
         link="plugins:nautobot_chatops:commandlog_list",
         name="Command Logs",
+        weight=400,
         permissions=["nautobot_chatops.view_commandlog"],
     ),
     *grafana_items,
@@ -48,7 +34,9 @@ items = [
 
 menu_items = (
     NavMenuTab(
-        name="Plugins",
-        groups=(NavMenuGroup(name="Nautobot ChatOps", items=tuple(items)),),
+        name="Apps",
+        icon=NavigationIconChoices.APPS,
+        weight=NavigationWeightChoices.APPS,
+        groups=(NavMenuGroup(name="Nautobot ChatOps", weight=200, items=tuple(items)),),
     ),
 )
