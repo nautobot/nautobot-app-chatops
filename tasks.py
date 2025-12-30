328b2a8b3f1053e6d125007a2f27303230ada202
bakebot <info@networktocode.com>
2025-12-30 17:52:15 +0000

    Cookie updated targeting develop by NetworkToCode Cookie Drift Manager Tool
    ## [3.0.0 (2025-12-30)](https://github.com/nautobot/cookiecutter-nautobot-app/releases/tag/3.0.0)
    
    ### Added
    
    - [#291](https://github.com/nautobot/cookiecutter-nautobot-app/issues/291) - Added support for Python `3.13`.
    
    ### Changed
    
    - [#291](https://github.com/nautobot/cookiecutter-nautobot-app/issues/291) - Updated minimum Nautobot version to `3.0.0`.
    - [#291](https://github.com/nautobot/cookiecutter-nautobot-app/issues/291) - Updated maximum Nautobot version to `<4.0.0`.
    - [#309](https://github.com/nautobot/cookiecutter-nautobot-app/issues/309) - Change the tag-nautobot-app workflow to support multiple nautobot-app versions.
    - [#320](https://github.com/nautobot/cookiecutter-nautobot-app/issues/320) - Upstream testing now runs against selected branches.
    
    ### Documentation
    
    - [#319](https://github.com/nautobot/cookiecutter-nautobot-app/issues/319) - Fixed nav typo and added missing 2.7.2 release notes.
    
    ### Housekeeping
    
    - [#322](https://github.com/nautobot/cookiecutter-nautobot-app/issues/322) - Updated CI workflow to always regenerate poetry lockfile.
    
    **Full Changelog**: https://github.com/nautobot/cookiecutter-nautobot-app/compare/nautobot-app-v2.7.2...nautobot-app-v3.0.0

--- tasks.py
+++ tasks.py
@@ -52,7 +52,7 @@ namespace = Collection("nautobot_chatops")
 namespace.configure(
     {
         "nautobot_chatops": {
-            "nautobot_ver": "2.4.20",
+            "nautobot_ver": "3.0.0",
             "project_name": "nautobot-chatops",
             "python_ver": "3.12",
             "local": False,
